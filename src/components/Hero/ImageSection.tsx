5f0872f2c971d4a269a13ef22d7368d23375428a
Henrique Nunes <hrn2001@gmail.com>
2025-11-08 19:18:44 +0000

    Change page logo

--- src/components/Hero/ImageSection.tsx
+++ src/components/Hero/ImageSection.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
 const ImageSection = () => {
+  // <div className="max-auto bg-gradient-1 absolute top-0 right-0 left-0 -z-10 aspect-square w-full rounded-full"></div>
   return (
     <div className="wow fadeInUp relative z-10 mx-auto w-full max-w-[550px] lg:mr-0">
-      <div className="max-auto bg-gradient-1 absolute top-0 right-0 left-0 -z-10 aspect-square w-full rounded-full"></div>
       <Image 
         src="/img/logo/fire_money.png"
         width={500}
